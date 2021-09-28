bc4827c7dba173af23f2e5f24b3b448ad47736e3
Krista Karppinen <krista.celestia@gmail.com>
2021-09-28 21:59:52 +0200

    Simple adventure selector

--- haskventure.hs
+++ haskventure.hs
@@ -1,1 +1,20 @@
-main = putStrLn "Welcome to Haskventure!"
+getAdventures :: [String]
+getAdventures = ["Sample adventure", "Simple adventure"]
+
+printList :: [String] -> IO [()]
+printList items = sequence $ map putStrLn items
+
+multipleChoice :: [String] -> IO String
+multipleChoice choices = do
+    printList [(show i) ++ ". " ++ x | (i,x) <- zip [1..] choices]
+    putStr "\nPlease make a selection: "
+    getLine
+
+main = do
+    putStrLn "Welcome to Haskventure!\n"
+    putStrLn "These are the available adventures:"
+    --adventures <- getAdventures
+    --myAdventure <- multipleChoice adventures
+    myAdventure <- multipleChoice $ getAdventures
+    putStrLn $ "You chose " ++ myAdventure
+
