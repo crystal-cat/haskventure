83aca48577a9768bfd435180061796a9ca41fe44
Krista Karppinen <krista.celestia@gmail.com>
2021-09-29 14:51:14 +0200

    Input validation for multiple select

--- haskventure.hs
+++ haskventure.hs
@@ -4,11 +4,17 @@ getAdventures = ["Sample adventure", "Simple adventure"]
 printList :: [String] -> IO [()]
 printList items = sequence $ map putStrLn items
 
+getChoice :: [String] -> IO String
+getChoice valid_opts = do
+    putStr "\nPlease make a selection: "
+    choice <- getLine
+    if choice `elem` valid_opts then return choice else getChoice valid_opts
+
 multipleChoice :: [String] -> IO String
 multipleChoice choices = do
-    printList [(show i) ++ ". " ++ x | (i,x) <- zip [1..] choices]
-    putStr "\nPlease make a selection: "
-    getLine
+    let enumChoices = zip (map show [1..]) choices
+    printList [i ++ ". " ++ x | (i,x) <- enumChoices]
+    getChoice . fst $ unzip enumChoices
 
 main = do
     putStrLn "Welcome to Haskventure!\n"
